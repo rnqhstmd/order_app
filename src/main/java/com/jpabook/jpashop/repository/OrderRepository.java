6838af60de82d35629d5bd585dda8c6f35764e90
BonSeung <bs.koo@sqisoft.com>
2024-07-01 18:31:26 +0900

    feat: 주문 레포지토리 메서드 추가 - #5

--- src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.jpabook.jpashop.repository;
 
 import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    List<Order> findAll(OrderSearch orderSearch);
 }
